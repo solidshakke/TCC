20009c0b7f784db266c0cbd0e1cb8af2417d8ef3
solidshakke <52498350+solidshakke@users.noreply.github.com>
2024-11-29 17:50:37 -0300

    Add files via upload

--- TCCII/tcc-project/src/pages/About/About.jsx
+++ TCCII/tcc-project/src/pages/About/About.jsx
@@ -1,12 +1,14 @@
 import styles from "./About.module.css";
-import { Link } from "react-router-dom";
+
 
 const About = () => {
   return (
     <div className={styles.about}>
-      <p>
-        Projeto de TCC
-      </p>
+      <h1>Projeto de TCC </h1>
+      <p>Desenvolvimento de Software em Javascript com React para Otimização de
+        Processos de Importação no Comércio Exterior: Integração de 
+          Funcionalidades Específicas para Importações </p>
+      <p>Elaborado por Leandro Aleksander Szulha para a disciplina de Trabalho de conclusão de curso II módulo 54/2024</p>
     </div>
   );
 };
